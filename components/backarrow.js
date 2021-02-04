39e9135b5e9b81172cb5f0e1a94f09f0cfad297d
Istvan Polacsek <stepan.polachek@gmail.com>
2021-02-04 21:22:43 +0100

    introduced button tooltips and cookiemodal timeout method

--- components/backarrow.js
+++ components/backarrow.js
@@ -3,17 +3,26 @@ import { FiArrowLeft } from 'react-icons/fi';
 import Button from 'react-bootstrap/Button';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from'react-bootstrap/OverlayTrigger';
+import { useState } from 'react';
 
 const BackArrow = () => {
-    
+
     const router = useRouter();
     const goBack = () => {
         router.back();
     }
 
+    const [visible, setVisible] = useState(true);
+
+    const toggleVis = () => {
+        
+        visible ? setVisible(false) : setVisible(true)
+        console.log(visible);
+    }
+
     return (
-        <OverlayTrigger placement="right" delay={{ show: 250, hide: 250 }} overlay={<Tooltip>Back</Tooltip>}> 
-            <Button size="lg" style={{paddingLeft: 25, paddingRight: 25}} variant="outline-dark" onClick={goBack}><FiArrowLeft/></Button>
+        <OverlayTrigger defaultShow={!visible} show={!visible} onToggle={toggleVis} placement="right" delay={{ show: 250, hide: 250 }} overlay={<Tooltip>Back</Tooltip>}> 
+            <Button size="lg" style={{paddingLeft: 25, paddingRight: 25}} variant="outline-dark" onClick={toggleVis, goBack}><FiArrowLeft/></Button>
         </OverlayTrigger>
       );
 }
