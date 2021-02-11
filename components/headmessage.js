bd222c6387de2c2d8b74d29c7dff5c3b1150c581
Istvan Polacsek <stepan.polachek@gmail.com>
2021-02-11 22:24:35 +0100

    introduced pwa

--- components/headmessage.js
+++ components/headmessage.js
@@ -19,19 +19,20 @@ const HeadMessage = (props) => {
             <meta name="msapplication-starturl" content="/" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <link rel="icon" type="image/png" sizes="192x192" href="android-icon-192x192.png" />
-            <link rel="apple-touch-icon" type="image/jpg" sizes="57x57" href="apple-icon-57x57.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="60x60" href="apple-icon-60x60.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="72x72" href="apple-icon-72x72.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="76x76" href="apple-icon-76x76.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="114x114" href="apple-icon-114x114.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="120x120" href="apple-icon-120x120.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="144x144" href="apple-icon-144x144.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="152x152" href="apple-icon-152x152.jpg"></link>
-            <link rel="apple-touch-icon" type="image/jpg" sizes="180x180" href="apple-icon-180x180.jpg"></link>
-            <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"></link>
-            <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"></link>
-            <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png"></link>            
-            <link rel="icon" type="image/png" sizes="512x512" href="favicon-512x512.png"></link>
+            <link rel="apple-touch-icon" type="image/jpg" sizes="57x57" href="apple-icon-57x57.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="60x60" href="apple-icon-60x60.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="72x72" href="apple-icon-72x72.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="76x76" href="apple-icon-76x76.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="114x114" href="apple-icon-114x114.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="120x120" href="apple-icon-120x120.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="144x144" href="apple-icon-144x144.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="152x152" href="apple-icon-152x152.jpg" />
+            <link rel="apple-touch-icon" type="image/jpg" sizes="180x180" href="apple-icon-180x180.jpg" />
+            <link rel="apple-touch-startup-image" type="image/jpg" href="favicon-512x512.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="512x512" href="favicon-512x512.png" />
             <title>{title}</title>
         </Head>
         </>
